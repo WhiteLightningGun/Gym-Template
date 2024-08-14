a78c793bd55d6f3d8dbf8564efee6de11c4e4a99
WhiteLightningGun <109026202+WhiteLightningGun@users.noreply.github.com>
2024-08-14 23:32:31 +0100

    slight alteration

--- js/gallery.js
+++ js/gallery.js
@@ -2,16 +2,18 @@ $(document).ready(function () {
   // Function to fade transition between images
   function fadeTransition(newImageSrc) {
     $("#galleryImg").fadeOut(600, function () {
-      $("#galleryImg").attr("src", newImageSrc).fadeIn(600);
+      $(this).css("display", "none");
+      $(this).attr("src", newImageSrc);
+      $(this).fadeIn(600);
     });
   }
 
-  // Attach click event to each paragraph
+  // Attach click event to paragraph
   $("p[data-image]").click(function () {
     var newImageSrc = $(this).data("image");
     fadeTransition(newImageSrc);
 
-    // Remove gallery-inactive class from the clicked element
+    // Remove gallery-inactive class from clicked element
     $(this).removeClass("gallery-inactive");
 
     // Add gallery-inactive class to all other elements
